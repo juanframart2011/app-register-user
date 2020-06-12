2b88de16905e763984bc1fbb3a6e4f33e068476d
juanframart2011 <juanframart2011@gmail.com>
2020-06-11 20:14:28 -0500

    finish sincronizar

--- src/app/page/syncup/syncup.page.ts
+++ src/app/page/syncup/syncup.page.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { user } from "../../interface/user";
 
 @Component({
   selector: 'app-syncup',
   templateUrl: 'syncup.page.html',
   styleUrls: ['syncup.page.scss']
 })
-export class SyncUpPage {
+export class SyncUpPage{
 
-  constructor() {}
+    isLoading = false;
+    syncUpWithout:number = 0;
+    listUserCurrent:user[];
 
+    constructor(
+        public alertController: AlertController,
+        public loadingController: LoadingController
+    ){
+
+        this.openLoading();
+        this._getList();
+    }
+
+    ionViewWillEnter(){
+
+        this.openLoading();
+        this._getList();
+    }
+
+    _getList(){
+
+    	let listUser = JSON.parse( localStorage.getItem( "list-user" ) );
+
+        var listUserExist = [];
+        
+		for( var u = 0; u < listUser.list.length; u++ ){
+            
+			listUserExist.push( listUser.list[u] );
+        }
+
+        this.listUserCurrent = listUserExist;
+        this.syncUpWithout = this.listUserCurrent.length;
+        this._closeLoading();
+    }
+
+    async _closeLoading(){
+        
+        this.isLoading = false;
+        return await this.loadingController.dismiss().then();
+    }
+
+    async openLoading() {
+        
+        this.isLoading = true;
+        return await this.loadingController.create({
+            message: 'Cargando Información'
+        }).then(a => {
+            a.present().then(() => {
+                if (!this.isLoading) {
+                    a.dismiss().then();
+                }
+            });
+        });
+    }
 }
